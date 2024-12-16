710f81c6a6fd2d322eea5119eb77d1489afdfda6
Victoria Lara <victoriap99@gmail.com>
2024-12-16 11:04:59 -0300

    Construção do CRUD categoria

---
+++ src/main/java/com/generation/projeto_final_bloco_02/model/Categoria.java
@@ -0,0 +1,37 @@
+package com.generation.projeto_final_bloco_02.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull(message = "A descrição da categoria é obrigatória")
+	private String descricao;
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return this.descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
